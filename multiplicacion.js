b1b598d76fe17f802176f78afa535a31f648e475
lucianaporto <lucianaporto0687@gmail.com>
2023-05-26 22:26:53 -0300

    Agrega ejercicio multiplicacion

--- multiplicacion.js
+++ multiplicacion.js
@@ -1,5 +1,11 @@
-let numero1 = 2;
-let numero2 = 17;
-let numero3 = 6;
-let multiplicacion = numero1 * numero2 * numero3;
+let num1;
+let num2;
+let num3;
+let multiplicacion = num1*num2*num3;
+console.log(multiplicacion);
+
+num1 = 2;
+num2 = 17;
+num3 = 6;
+multiplicacion = num1*num2*num3;
 console.log(multiplicacion);
