ee663be99f1d09afa9a5197f7a17e7287151e9e0
lucianaporto <lucianaporto0687@gmail.com>
2023-05-26 23:50:49 -0300

    Agrego el archivo conversionGrados.js

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,11 @@
+let fahrenheit;
+let celsius = (fahrenheit - 32) * 5/9;
+console.log(celsius);
+
+fahrenheit = 50;
+celsius = (fahrenheit - 32) * 5/9;
+console.log(celsius);
+
+fahrenheit = 75;
+celsius = (fahrenheit - 32) * 5/9;
+console.log(celsius);
