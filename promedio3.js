0ebf9dfab8c0c641138b733ae5ad924fd9e17b7b
lucianaporto <lucianaporto0687@gmail.com>
2023-05-26 22:55:05 -0300

    Agrega ejercicio promedio3

---
+++ promedio3.js
@@ -0,0 +1,23 @@
+let numX;
+let numY;
+let numZ;
+let promedio = (numX + numY + numZ) /3;
+console.log(promedio);
+
+numX = 5;
+numY = 7;
+numZ = 9;
+promedio = (numX + numY + numZ) /3;
+console.log(promedio);
+
+numX = -2;
+numY = -3;
+numZ = -8;
+promedio = (numX + numY + numZ) /3;
+console.log(promedio);
+
+numX = -1.2;
+numY = 5.3;
+numZ = -3.3;
+promedio = (numX + numY + numZ) /3;
+console.log(promedio);
