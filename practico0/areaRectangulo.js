fd149cfdb51ca1cc9bb9a8017fd9f6ab1f5f52b1
lucianaporto <lucianaporto0687@gmail.com>
2023-06-01 20:02:35 -0300

    Práctino N0 finalizado

--- practico0/areaRectangulo.js
+++ practico0/areaRectangulo.js
@@ -1,14 +1,4 @@
-let base;
-let altura;
+let base = 5;
+let altura = 3;
 let area = base * altura;
-console.log(area);
-
-base = 5;
-altura = 3;
-area = base * altura;
-console.log(area);
-
-base = 12.5;
-altura = 5.3;
-area = base * altura;
 console.log(area);
