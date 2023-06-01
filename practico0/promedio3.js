13f3da64af0e0304a1aa62397823142359e6c878
lucianaporto <lucianaporto0687@gmail.com>
2023-06-01 19:59:48 -0300

    Práctico N0 finalizado

--- practico0/promedio3.js
+++ practico0/promedio3.js
@@ -1,23 +1,5 @@
-let numX;
-let numY;
-let numZ;
+let numX = 5;
+let numY = 9;
+let numZ = 7;
 let promedio = (numX + numY + numZ) /3;
-console.log(promedio);
-
-numX = 5;
-numY = 7;
-numZ = 9;
-promedio = (numX + numY + numZ) /3;
-console.log(promedio);
-
-numX = -2;
-numY = -3;
-numZ = -8;
-promedio = (numX + numY + numZ) /3;
-console.log(promedio);
-
-numX = -1.2;
-numY = 5.3;
-numZ = -3.3;
-promedio = (numX + numY + numZ) /3;
 console.log(promedio);
